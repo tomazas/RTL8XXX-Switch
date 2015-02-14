c087e8ddf59f5f201f82c169d0a92b3b962e404a
Jia Jun Tan <jiajun.tan@gmailcom>
2015-02-14 12:55:18 +0800

    added dump_eeprom_24c16.py

---
+++ scripts/dump_eeprom_24c16.py
@@ -0,0 +1,41 @@
+from pyBusPirateLite.I2Chigh import *
+import struct 
+
+EEPROM_SIZE = 16384
+OUTPUT_FILE = "eeprom_dump.bin"
+
+i2c = I2Chigh("COM13", 115200, 5)
+i2c.BBmode()
+i2c.enter_I2C()
+i2c.cfg_pins(I2CPins.POWER | I2CPins.PULLUPS)
+
+#reset current address
+i2c.send_start_bit()
+i2c.bulk_trans(2, [0xa0, 0x00])
+i2c.send_start_bit()
+i2c.bulk_trans(1, [0xa1])
+r = i2c.read_byte() #value at addr 0x0
+r = ord(r)
+i2c.send_stop_bit()
+print "%X%X: %X"%(0xa1,0x0,r)
+dump = [r]
+
+# read the rest
+for i in range((EEPROM_SIZE/8)-1):
+   addr_control_byte = (i>>8)&0b111
+   devSelect = 0xa1 | (addr_control_byte<<1)
+   addr = i & 0xff
+   i2c.send_start_bit()
+   i2c.bulk_trans(1, [devSelect])   
+   r = i2c.read_byte()
+   r = ord(r)
+   i2c.send_stop_bit()
+   print "%X%X: %X"%(devSelect,addr,r)
+   dump.append(r)
+
+#write to fie
+print "Writing to file: %s"%OUTPUT_FILE
+f = open(OUTPUT_FILE,"wb")
+struct_fmt = "%dB"%len(dump)
+f.write(struct.pack(struct_fmt, *dump))
+f.close()
